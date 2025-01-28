98e9c45663c867eb6f2eaf79337cf7b0978098e2
Raphaela <bmraphaela@gmail.com>
2025-01-28 13:59:26 -0300

    :sparkles: feat: estudando DOM

---
+++ RevisãoJs/ManipulaçãoDOM/aula-01.js
@@ -0,0 +1,6 @@
+document.title = "Manipulação - DOM (alterado)";
+
+document.body.style.background = "#FC5C7D";
+// document.body.style.backgroundImage = "url('Sublime Light.jpg')";
+
+document.write("<h1>Olá Mundo!</h1>");
